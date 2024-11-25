95e662e4c132a59043b528e1fa936e640e76fa5c
罗堃 <forestluo@gmail.com>
2024-11-25 16:51:49 +0800

    尝试一些新方法；修正了一些问题。

--- Word2Vector.py
+++ Word2Vector.py
@@ -514,7 +514,7 @@ class VectorGroup(ContentGroup) :
             pb.increase()
             # 检查计数
             # 至少需要的记录次数
-            if item.count >= 256 :
+            if item.count >= 128 :
                 continue
             # 将无效数据加入删除队列中
             if item.content not in removed :
